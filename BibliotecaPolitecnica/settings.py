8ac62d38102fd30563948cd4c67771776072b51c
Guidzero <guidopr27@hotmail.com>
2023-06-26 08:11:45 -0600

    Arreglando settings

--- BibliotecaPolitecnica/settings.py
+++ BibliotecaPolitecnica/settings.py
@@ -140,9 +140,9 @@ if not DEBUG:    # Tell Django to copy statics to the `staticfiles` directory
     # and creating unique names for each version so they can safely be cached forever.
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'BibliotecaPolitecnica/static'),)
+# STATICFILES_DIRS = (os.path.join(BASE_DIR, 'BibliotecaPolitecnica/static'),)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
