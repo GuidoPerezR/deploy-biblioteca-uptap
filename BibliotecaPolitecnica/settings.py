c4fb048b69fc8fd1a8d1c6db0b6e9ffc86418860
Guidzero <guidopr27@hotmail.com>
2023-06-26 08:37:04 -0600

    Arreglando settings

--- BibliotecaPolitecnica/settings.py
+++ BibliotecaPolitecnica/settings.py
@@ -142,7 +142,7 @@ if not DEBUG:    # Tell Django to copy statics to the `staticfiles` directory
 
 # STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-# STATICFILES_DIRS = (os.path.join(BASE_DIR, 'BibliotecaPolitecnica/static'),)
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'BibliotecaPolitecnica/static'),)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
